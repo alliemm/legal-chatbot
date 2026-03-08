b94649db27ed8b8e4dc04e0f01b77458e2cc9a57
JtheSloth <jsd9975@nyu.edu>
2026-03-08 17:54:21 -0400

    Did backend for survey and added ability to delete account

--- backend/backend.cpp
+++ backend/backend.cpp
@@ -36,7 +36,7 @@ int main() {
 
     //signup
     CROW_ROUTE(app, "/signup").methods("POST"_method)
-    ([connectionString](const crow::request& req){
+    ([&app, connectionString](const crow::request& req){
         auto data = crow::json::load(req.body);
         //if there is no data or if the data is missing the email or password field return error
         if (!data || !data.has("password") || !data.has("email"))
@@ -74,6 +74,8 @@ int main() {
             //end transaction
             transaction.commit();
             //store the current user as authenticated
+            auto& session = app.get_context<Session>(req);
+            session.set("user", std::string(email));
             return crow::response(201, "Created new account");
         }else
         {
@@ -100,7 +102,7 @@ int main() {
         if (checkPassword(email,password, connectionString))
         {
             auto& session = app.get_context<Session>(req);
-            session.set("user", std::string(email.c_str()));
+            session.set("user", std::string(email));
 
             return crow::response(202, "User logged in");
         }else
@@ -123,6 +125,76 @@ int main() {
         //return success
         return crow::response(200, "User logged out");
     });
+    //delete account
+    CROW_ROUTE(app, "/deactivate").methods("DELETE"_method)
+   ([&app, connectionString](const crow::request& req){
+       auto& session = app.get_context<Session>(req);
+       std::string email = session.get("user", "");
+       //if the user isn't logged in then respond with error
+       if (email.empty())
+       {
+           return crow::response(401, "Unauthorized");
+       }
+       //else delete the user from all tables in the database
+       //connect to database
+       auto conn = connectToDatabase(connectionString);
+       if (!conn)
+       {
+           return crow::response(500, "Failed to connect to database");
+       }
+       //start a transaction
+       pqxx::work transaction(*conn);
+       //run query to delete from all databases
+       pqxx::result res = transaction.exec("DELETE FROM users WHERE email = $1",pqxx::params{email});
+       res = transaction.exec("DELETE FROM surveys WHERE email = $1",pqxx::params{email});
+       res = transaction.exec("DELETE FROM documents WHERE email = $1",pqxx::params{email});
+       //commit the transaction
+       transaction.commit();
+       return crow::response(200, "Account Deleted");
+   });
+
+    CROW_ROUTE(app, "/survey").methods("POST"_method)
+  ([&app, connectionString](const crow::request& req){
+      //ensure that user is logged in
+      auto& session = app.get_context<Session>(req);
+      std::string email = session.get("user", "");
+      //if the user isn't logged in then respond with error
+      if (email.empty())
+      {
+          return crow::response(401, "Unauthorized");
+      }
+      //get the body of the request
+      auto data = crow::json::load(req.body);
+      //if there is no data or if the data is missing any of the necessary fields return an error
+      if (!data || !data.has("userType") || !data.has("workedWithLawyer") || !data.has("howOften") || !data.has("typesOfDocuments")
+          || !data.has("biggestConcerns") || !data.has("jargonUnderstanding") || !data.has("outcome"))
+      {
+          return crow::response(400, "Missing survey information");
+      }
+      //get survey answers from request
+      std::string userType= data["userType"].s();
+      std::string workedWithLawyer = data["workedWithLawyer"].s();
+      std::string howOften = data["howOften"].s();
+      std::string typesOfDocuments = data["typesOfDocuments"].s();
+      std::string biggestConcerns = data["biggestConcerns"].s();
+      std::string jargonUnderstanding = data["jargonUnderstanding"].s();
+      std::string outcome = data["outcome"].s();
+      //connect to database
+      auto conn = connectToDatabase(connectionString);
+      if (!conn)
+      {
+          return crow::response(500, "Failed to connect to database");
+      }
+      //start a transaction
+      pqxx::work transaction(*conn);
+      //run query to add user survey answers to database
+      pqxx::result res = transaction.exec("INSERT INTO surveys VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+          pqxx::params{email, userType, workedWithLawyer, howOften, typesOfDocuments, biggestConcerns, jargonUnderstanding, outcome});
+      //commit the transaction
+      transaction.commit();
+      return crow::response(200, "Saved survey results");
+  });
+
 
     app.port(18080).multithreaded().run();
     //app.port(18080).run();
