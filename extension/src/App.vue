6836e4d1f49d3b20bd3d489e49d01824d138c443
saniaawale <ska7008@nyu.edu>
2026-05-03 17:22:51 -0400

    trying to fix popup connection to background.js

--- extension/src/App.vue
+++ extension/src/App.vue
@@ -1,14 +1,26 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import UnlockScreen from "./components/UnlockScreen.vue";
 import DetectedScreen from "./components/DetectedScreen.vue";
 import ChatScreen from "./components/ChatScreen.vue";
 import ClausesScreen from "./components/ClausesScreen.vue";
 import SummaryScreen from "./components/SummaryScreen.vue";
 
-const EMAIL = "saunter@getalby.com";
+const EMAIL = localStorage.getItem("user_token");
 const screen = ref("unlock");
 const go = (target) => (screen.value = target);
+
+onMounted(() => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const tabId = tabs[0]?.id;
+    if (tabId == null) return;
+    chrome.runtime.sendMessage({ type: "GET_STATUS", tabId }, (response) => {
+      if (response?.detected) {
+        screen.value = "detected";
+      }
+    });
+  });
+});
 </script>
 
 <template>
